5fad89675dbc0d235b90acc7a02c41e5b0ab12ce
ArdHas20 <ardhas20@htl-shkoder.com>
2026-03-04 12:28:50 +0100

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,24 @@
+
+<script>
+
+import background from '$lib/assets/foto.jpg'
+
+</script>
+
+
+<h1>Electronic Devices</h1>
+
+
+<header class="h-[500px] bg-center bg-cover flex items-center justify-center"
+style="background-image:url({background})">
+
+<h1 class="text-5xl">Electronic Devices</h1>
+</header>
+
+<main>
+
+</main>
+
+<footer>
+
+</footer>
